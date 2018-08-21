c9430eed0f104d7cfcdb179688aacc6345e0bc08
Vinay <ksvinaysai@gmail.com>
2018-08-21 22:36:10 +0530

    Task Again.

--- src/com/task/all/TodaysTask.java
+++ src/com/task/all/TodaysTask.java
@@ -5,33 +5,29 @@ import java.util.Scanner;
 
 public class TodaysTask {
 
-	// This is a merge method.Which will sort and add two arrays into third.
-	public static void mergeArrays(int[] a, int[] b, int n, int m, int[] arr3) {
-		int i = 0, j = 0, k = 0;
+	// This method can sort any individual array.
+	public void cansortanyarray(int a[]) {
 
-		// Traverse both array
-		while (i < n && j < m) {
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length; j++) {
+				if (a[i] < a[j]) {
 
-			// To check which array is greater.
-			if (a[i] < b[j])
-				// Store elements accordingly.
-				arr3[k++] = a[i++];
-			else
-				// Store elements accordingly.
-				arr3[k++] = b[j++];
+					int temp;
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+
+				}
+			}
+
 		}
-
-		// Store remaining elements of first array
-		while (i < n)
-			arr3[k++] = a[i++];
-
-		// Store remaining elements of second array
-		while (j < m)
-			arr3[k++] = b[j++];
+		System.out.println("Sorted array is:" + Arrays.toString(a));
 	}
 
-	public static void main(String[] args) {
-		int n, m, temp;
+	// taking two arrays and merging them into third.
+	public void sample() {
+
+		int n, m;
 		Scanner s = new Scanner(System.in);
 		System.out.print("Enter the no. of elements you want in array a:");
 		n = s.nextInt();
@@ -39,67 +35,38 @@ public class TodaysTask {
 		m = s.nextInt();
 		int a[] = new int[n];
 		int b[] = new int[m];
-		System.out.println("Enter all the elements a:");
-		for (int i = 0; i < n; i++) {
-			a[i] = s.nextInt();
-		}
+		int ab[] = new int[n + m];
 
-		System.out.println("Enter all the elements b:");
-		for (int i = 0; i < m; i++) {
+		System.out.println("Enter all the elements a and b:");
+
+		for (int i = 0; i < n & i < m; i++) {
+			a[i] = s.nextInt();
+
 			b[i] = s.nextInt();
 
 		}
-		System.out.println(
-				"Your first array before sorting is:::" + Arrays.toString(a) + "\nSecond array" + Arrays.toString(b));
 
-		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
-				if (a[i] > a[j]) {
-					temp = a[i];
-					a[i] = a[j];
-					a[j] = temp;
-				}
+		int l = 0, k = 0;
 
-			}
-		}
+		for (int i = 0; i < ab.length; i++) {
 
-		for (int i = 0; i < m; i++) {
-			for (int j = i + 1; j < m; j++) {
-				if (b[i] > b[j]) {
-					temp = b[i];
-					b[i] = b[j];
-					b[j] = temp;
-				}
-
-			}
-		}
-		System.out.print("\nfirst element in After Sorting::");
-		for (int i = 0; i < n - 1; i++) {
-			System.out.print(a[i] + ",");
-			
-		}
-		System.out.print(a[n - 1]);
-		System.out.print("\nSecond Element After Sorting::");
-		for (int i = 0; i < m - 1; i++) {
-			System.out.print(b[i] + ",");
+			if (i % 2 == 0)
+				ab[i] = a[l++];
+			else
+				ab[i] = b[k++];
 
 		}
 
-		System.out.print(b[m - 1]);
+		System.out.println("First array is:" + Arrays.toString(a) + "\nSecond array is:" + Arrays.toString(b)
+				+ "\nab array is:" + Arrays.toString(ab));
 
-		System.out.println("\n_______________________");
-
-		int length = a.length + b.length;
-		int merge[] = new int[length];
-
-		mergeArrays(a, b, n, m, merge);
-
-		System.out.println("\nArray after merging");
-		for (int i = 0; i < n + m; i++)
-
-			System.out.print(merge[i] + " ");
-
+		// Sorting array ab.
+		new TodaysTask().cansortanyarray(ab);
 		s.close();
 	}
 
+	public static void main(String[] args) {
+
+		new TodaysTask().sample();
+	}
 }
